33a4ba08157b07e2e837b272212afd596b0244f2
Mykola Zhyhallo <mzhyhall@gmail.com>
2023-02-25 16:53:01 +0100

    Added native_iam_policy exist tests

---
+++ modules/native/testing/integration/iam/policy/exist_test.go
@@ -0,0 +1,172 @@
+package namespace
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+
+	native "github.com/slamy-solutions/openbp/modules/native/libs/golang"
+	"github.com/slamy-solutions/openbp/modules/native/libs/golang/iam/policy"
+	"github.com/slamy-solutions/openbp/modules/native/libs/golang/namespace"
+	"github.com/slamy-solutions/openbp/modules/native/testing/tools"
+)
+
+type ExistTestSuite struct {
+	suite.Suite
+
+	nativeStub *native.NativeStub
+}
+
+func (suite *ExistTestSuite) SetupSuite() {
+	suite.nativeStub = native.NewNativeStub(native.NewStubConfig().WithNamespaceService().WithIAMPolicyService())
+	err := suite.nativeStub.Connect()
+	if err != nil {
+		panic(err)
+	}
+}
+func (suite *ExistTestSuite) TearDownSuite() {
+	suite.nativeStub.Close()
+}
+func TestExistTestSuite(t *testing.T) {
+	suite.Run(t, new(ExistTestSuite))
+}
+
+func (s *ExistTestSuite) TestExistInGlobalNamespace() {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	createResponse, err := s.nativeStub.Services.IamPolicy.Create(ctx, &policy.CreatePolicyRequest{
+		Namespace:            "",
+		Name:                 tools.GetRandomString(20),
+		Description:          tools.GetRandomString(20),
+		Managed:              &policy.CreatePolicyRequest_No{No: &policy.NotManagedData{}},
+		NamespaceIndependent: false,
+		Resources:            []string{},
+		Actions:              []string{},
+	})
+	require.Nil(s.T(), err)
+	defer s.nativeStub.Services.IamPolicy.Delete(context.Background(), &policy.DeletePolicyRequest{Namespace: "", Uuid: createResponse.Policy.Uuid})
+
+	existResponse, err := s.nativeStub.Services.IamPolicy.Exist(ctx, &policy.ExistPolicyRequest{
+		Namespace: "",
+		Uuid:      createResponse.Policy.Uuid,
+		UseCache:  true,
+	})
+	require.Nil(s.T(), err)
+
+	require.True(s.T(), existResponse.Exist)
+}
+
+func (s *ExistTestSuite) TestDoesntExistInGlobalNamespace() {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	createResponse, err := s.nativeStub.Services.IamPolicy.Create(ctx, &policy.CreatePolicyRequest{
+		Namespace:            "",
+		Name:                 tools.GetRandomString(20),
+		Description:          tools.GetRandomString(20),
+		Managed:              &policy.CreatePolicyRequest_No{No: &policy.NotManagedData{}},
+		NamespaceIndependent: false,
+		Resources:            []string{},
+		Actions:              []string{},
+	})
+	require.Nil(s.T(), err)
+	defer s.nativeStub.Services.IamPolicy.Delete(context.Background(), &policy.DeletePolicyRequest{Namespace: "", Uuid: createResponse.Policy.Uuid})
+
+	existResponse, err := s.nativeStub.Services.IamPolicy.Exist(ctx, &policy.ExistPolicyRequest{
+		Namespace: "",
+		Uuid:      primitive.NewObjectID().Hex(),
+		UseCache:  true,
+	})
+	require.Nil(s.T(), err)
+
+	require.False(s.T(), existResponse.Exist)
+}
+
+func (s *ExistTestSuite) TestExistInNamespace() {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	namespaceName := tools.GetRandomString(20)
+	_, err := s.nativeStub.Services.Namespace.Create(ctx, &namespace.CreateNamespaceRequest{
+		Name:        namespaceName,
+		FullName:    "",
+		Description: "",
+	})
+	require.Nil(s.T(), err)
+	defer s.nativeStub.Services.Namespace.Delete(context.Background(), &namespace.DeleteNamespaceRequest{Name: namespaceName})
+
+	createResponse, err := s.nativeStub.Services.IamPolicy.Create(ctx, &policy.CreatePolicyRequest{
+		Namespace:            namespaceName,
+		Name:                 tools.GetRandomString(20),
+		Description:          tools.GetRandomString(20),
+		Managed:              &policy.CreatePolicyRequest_No{No: &policy.NotManagedData{}},
+		NamespaceIndependent: false,
+		Resources:            []string{},
+		Actions:              []string{},
+	})
+	require.Nil(s.T(), err)
+	defer s.nativeStub.Services.IamPolicy.Delete(context.Background(), &policy.DeletePolicyRequest{Namespace: namespaceName, Uuid: createResponse.Policy.Uuid})
+
+	existResponse, err := s.nativeStub.Services.IamPolicy.Exist(ctx, &policy.ExistPolicyRequest{
+		Namespace: namespaceName,
+		Uuid:      createResponse.Policy.Uuid,
+		UseCache:  true,
+	})
+	require.Nil(s.T(), err)
+
+	require.True(s.T(), existResponse.Exist)
+}
+
+func (s *ExistTestSuite) TestDoesntExistInNamespace() {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	namespaceName := tools.GetRandomString(20)
+	_, err := s.nativeStub.Services.Namespace.Create(ctx, &namespace.CreateNamespaceRequest{
+		Name:        namespaceName,
+		FullName:    "",
+		Description: "",
+	})
+	require.Nil(s.T(), err)
+	defer s.nativeStub.Services.Namespace.Delete(context.Background(), &namespace.DeleteNamespaceRequest{Name: namespaceName})
+
+	createResponse, err := s.nativeStub.Services.IamPolicy.Create(ctx, &policy.CreatePolicyRequest{
+		Namespace:            namespaceName,
+		Name:                 tools.GetRandomString(20),
+		Description:          tools.GetRandomString(20),
+		Managed:              &policy.CreatePolicyRequest_No{No: &policy.NotManagedData{}},
+		NamespaceIndependent: false,
+		Resources:            []string{},
+		Actions:              []string{},
+	})
+	require.Nil(s.T(), err)
+	defer s.nativeStub.Services.IamPolicy.Delete(context.Background(), &policy.DeletePolicyRequest{Namespace: namespaceName, Uuid: createResponse.Policy.Uuid})
+
+	existResponse, err := s.nativeStub.Services.IamPolicy.Exist(ctx, &policy.ExistPolicyRequest{
+		Namespace: namespaceName,
+		Uuid:      primitive.NewObjectID().Hex(),
+		UseCache:  true,
+	})
+	require.Nil(s.T(), err)
+
+	require.False(s.T(), existResponse.Exist)
+}
+
+func (s *ExistTestSuite) TestDoesntExistWhenNamespaceDoesntExist() {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	existResponse, err := s.nativeStub.Services.IamPolicy.Exist(ctx, &policy.ExistPolicyRequest{
+		Namespace: tools.GetRandomString(20),
+		Uuid:      primitive.NewObjectID().Hex(),
+		UseCache:  true,
+	})
+	require.Nil(s.T(), err)
+
+	require.False(s.T(), existResponse.Exist)
+}
