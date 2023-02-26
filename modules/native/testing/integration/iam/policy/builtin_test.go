1195fc7747f7c1cde8ff3dba5ad62d99df1f8be8
Mykola Zhyhallo <mzhyhall@gmail.com>
2023-02-26 22:55:43 +0100

    Added native_iam_policy builtin tests

--- modules/native/testing/integration/iam/policy/builtin_test.go
+++ modules/native/testing/integration/iam/policy/builtin_test.go
@@ -7,6 +7,8 @@ import (
 
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
 	native "github.com/slamy-solutions/openbp/modules/native/libs/golang"
 	"github.com/slamy-solutions/openbp/modules/native/libs/golang/iam/policy"
@@ -87,4 +89,19 @@ func (s *BuiltInTestSuite) TestGetForNamespaceEmpty() {
 		Type:      policy.BuiltInPolicyType_EMPTY,
 	})
 	require.Nil(s.T(), err)
+}
+
+func (s *BuiltInTestSuite) TestFailsWithNotFoundErrorForNonExistingNamespace() {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	_, err := s.nativeStub.Services.IamPolicy.GetBuiltInPolicy(ctx, &policy.GetBuiltInPolicyRequest{
+		Namespace: tools.GetRandomString(20),
+		Type:      policy.BuiltInPolicyType_EMPTY,
+	})
+	if st, ok := status.FromError(err); ok {
+		require.Equal(s.T(), codes.NotFound, st.Code())
+	} else {
+		require.Fail(s.T(), "Error wasnt exist")
+	}
 }
