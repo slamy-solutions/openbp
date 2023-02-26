9e05c42a97e5744c500253c3956cfd93e43bec00
Mykola Zhyhallo <mzhyhall@gmail.com>
2023-02-26 22:52:37 +0100

    Added native_iam_policy builtin tests

---
+++ modules/native/testing/integration/iam/policy/builtin_test.go
@@ -0,0 +1,90 @@
+package namespace
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+
+	native "github.com/slamy-solutions/openbp/modules/native/libs/golang"
+	"github.com/slamy-solutions/openbp/modules/native/libs/golang/iam/policy"
+	"github.com/slamy-solutions/openbp/modules/native/libs/golang/namespace"
+	"github.com/slamy-solutions/openbp/modules/native/testing/tools"
+)
+
+type BuiltInTestSuite struct {
+	suite.Suite
+
+	nativeStub *native.NativeStub
+}
+
+func (suite *BuiltInTestSuite) SetupSuite() {
+	suite.nativeStub = native.NewNativeStub(native.NewStubConfig().WithNamespaceService().WithIAMPolicyService())
+	err := suite.nativeStub.Connect()
+	if err != nil {
+		panic(err)
+	}
+}
+func (suite *BuiltInTestSuite) TearDownSuite() {
+	suite.nativeStub.Close()
+}
+func TestBuiltInTestSuite(t *testing.T) {
+	suite.Run(t, new(BuiltInTestSuite))
+}
+
+func (s *BuiltInTestSuite) TestGetForGlobalNamespace() {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	_, err := s.nativeStub.Services.IamPolicy.GetBuiltInPolicy(ctx, &policy.GetBuiltInPolicyRequest{
+		Namespace: "",
+		Type:      policy.BuiltInPolicyType_GLOBAL_ROOT,
+	})
+	require.Nil(s.T(), err)
+}
+
+func (s *BuiltInTestSuite) TestGetForNamespaceRoot() {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	namespaceName := tools.GetRandomString(20)
+	_, err := s.nativeStub.Services.Namespace.Create(ctx, &namespace.CreateNamespaceRequest{
+		Name:        namespaceName,
+		FullName:    "",
+		Description: "",
+	})
+	require.Nil(s.T(), err)
+	defer s.nativeStub.Services.Namespace.Delete(context.Background(), &namespace.DeleteNamespaceRequest{Name: namespaceName})
+
+	time.Sleep(time.Millisecond * 100)
+
+	_, err = s.nativeStub.Services.IamPolicy.GetBuiltInPolicy(ctx, &policy.GetBuiltInPolicyRequest{
+		Namespace: namespaceName,
+		Type:      policy.BuiltInPolicyType_NAMESPACE_ROOT,
+	})
+	require.Nil(s.T(), err)
+}
+
+func (s *BuiltInTestSuite) TestGetForNamespaceEmpty() {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	namespaceName := tools.GetRandomString(20)
+	_, err := s.nativeStub.Services.Namespace.Create(ctx, &namespace.CreateNamespaceRequest{
+		Name:        namespaceName,
+		FullName:    "",
+		Description: "",
+	})
+	require.Nil(s.T(), err)
+	defer s.nativeStub.Services.Namespace.Delete(context.Background(), &namespace.DeleteNamespaceRequest{Name: namespaceName})
+
+	time.Sleep(time.Millisecond * 100)
+
+	_, err = s.nativeStub.Services.IamPolicy.GetBuiltInPolicy(ctx, &policy.GetBuiltInPolicyRequest{
+		Namespace: namespaceName,
+		Type:      policy.BuiltInPolicyType_EMPTY,
+	})
+	require.Nil(s.T(), err)
+}
